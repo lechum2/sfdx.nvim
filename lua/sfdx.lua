aecbf14adaca765b8bb71292d2b776604e59a06e
Sebastian Lech <lechum2@gmail.com>
2022-03-22 12:23:50 +0100

    Use vim buildin function to execute sfdx command - it does not draw continous output under cursor

--- lua/sfdx.lua
+++ lua/sfdx.lua
@@ -1,14 +1,12 @@
 local M = {}
 
-local sfdxCommand = '/usr/bin/sfdx'
-local sfdxDeployCommand = 'force:source:deploy'
-local sfdxDeployPathParameter = '--sourcepath'
+local sfdxCommand = "/usr/bin/sfdx"
+local sfdxDeployCommand = "force:source:deploy"
+local sfdxDeployPathParameter = "--sourcepath"
 
 function M.deploy()
-	local command = { sfdxCommand, sfdxDeployCommand, sfdxDeployPathParameter, vim.fn.expand('%') }
-	local openPop = assert(io.popen(table.concat(command, ' '), 'r'))
-	local output = openPop:read('*all')
-	openPop:close()
+	local command = { sfdxCommand, sfdxDeployCommand, sfdxDeployPathParameter, vim.fn.expand("%") }
+	local output = vim.fn.system(table.concat(command, " "))
 	print(output)
 end
 
