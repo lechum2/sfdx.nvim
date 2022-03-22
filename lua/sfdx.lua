3bb53b272317920526a989185dd40e0f3d7c66ca
Sebastian Lech <lechum2@gmail.com>
2022-03-22 10:45:14 +0100

    Add function to deploy current file - continous output from command displays in a wrong place

--- lua/sfdx.lua
+++ lua/sfdx.lua
@@ -1,7 +1,15 @@
 local M = {}
 
+local sfdxCommand = '/usr/bin/sfdx'
+local sfdxDeployCommand = 'force:source:deploy'
+local sfdxDeployPathParameter = '--sourcepath'
+
 function M.deploy()
-	print('test from plugin')
+	local command = { sfdxCommand, sfdxDeployCommand, sfdxDeployPathParameter, vim.fn.expand('%') }
+	local openPop = assert(io.popen(table.concat(command, ' '), 'r'))
+	local output = openPop:read('*all')
+	openPop:close()
+	print(output)
 end
 
 return M
