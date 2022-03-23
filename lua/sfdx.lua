ed359f51e10239ef49303f8f3ff38e4294d1e322
Sebastian Lech <lechum2@gmail.com>
2022-03-23 10:50:45 +0100

    Add same simple way to run all apex tests in current file

--- lua/sfdx.lua
+++ lua/sfdx.lua
@@ -1,12 +1,23 @@
 local M = {}
 
 local sfdxCommand = "/usr/bin/sfdx"
-local sfdxDeployCommand = "force:source:deploy"
-local sfdxDeployPathParameter = "--sourcepath"
 
-function M.deploy()
+function M.deployCurrentFile()
+	local sfdxDeployCommand = "force:source:deploy"
+	local sfdxDeployPathParameter = "--sourcepath"
 	local command = { sfdxCommand, sfdxDeployCommand, sfdxDeployPathParameter, vim.fn.expand("%") }
 	print("deploying...")
+	local output = vim.fn.system(table.concat(command, " "))
+	print(output)
+end
+
+function M.runTestsFromCurrentFile()
+	local sfdxRunTestCommand = "force:apex:test:run"
+	local sfdxRunTestClassParamter = "--classnames"
+	local sfdxRunTestWaitParamter = "--wait 10"
+	local filename = vim.fn.expand("%:t:r")
+	local command = { sfdxCommand, sfdxRunTestCommand, sfdxRunTestClassParamter, filename, sfdxRunTestWaitParamter }
+	print("running tests...")
 	local output = vim.fn.system(table.concat(command, " "))
 	print(output)
 end
