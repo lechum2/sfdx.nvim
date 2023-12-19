7afad5b5222a9b27f8073b4d7a1756a8b0d7725f
Sebastian Lech <lechum2@gmail.com>
2023-12-19 14:42:09 +0100

    Convert to sf command

--- lua/sfdx.lua
+++ lua/sfdx.lua
@@ -1,45 +1,49 @@
 local M = {}
 
-local sfdxCommand = "sfdx"
+local sfdxCommand = "sf"
 
 function M.deployCurrentFile()
-	local sfdxDeployCommand = "force:source:deploy"
-	local sfdxDeployPathParameter = "--sourcepath"
-	local command = { sfdxCommand, sfdxDeployCommand, sfdxDeployPathParameter, vim.fn.expand("%") }
-	print("deploying...")
-	local output = vim.fn.system(table.concat(command, " "))
-	print(output)
+    local sfdxDeployCommand = "project deploy start"
+    local sfdxDeployPathParameter = "--source-dir"
+    local command = { sfdxCommand, sfdxDeployCommand, sfdxDeployPathParameter, vim.fn.expand("%") }
+    print("deploying...")
+    local output = vim.fn.system(table.concat(command, " "))
+    print(output)
 end
 
 function M.runTestsFromCurrentFile()
-	local sfdxRunTestCommand = "force:apex:test:run"
-	local sfdxRunTestClassParamter = "--classnames"
-	local sfdxRunTestWaitParamter = "--wait 10"
-	local filename = vim.fn.expand("%:t:r")
-	local command = { sfdxCommand, sfdxRunTestCommand, sfdxRunTestClassParamter, filename, sfdxRunTestWaitParamter }
-	print("running tests...")
-	local output = vim.fn.system(table.concat(command, " "))
-	print(output)
+    local sfdxRunTestCommand = "apex test run"
+    local sfdxRunTestClassParamter = "--class-names"
+    local sfdxRunTestWaitParamter = "--wait 10"
+    local filename = vim.fn.expand("%:t:r")
+    local command = { sfdxCommand, sfdxRunTestCommand, sfdxRunTestClassParamter, filename, sfdxRunTestWaitParamter }
+    print("running tests...")
+    local output = vim.fn.system(table.concat(command, " "))
+    print(output)
 end
 
 function M.retrieveCurrentFile()
-	local sfdxSubcommand = "force:source:retrieve"
-	local flagName = "--sourcepath"
-	local filename = vim.fn.expand("%:t:r")
-	local command = { sfdxCommand, sfdxSubcommand, flagName, filename }
-	print("retrieving...")
-	local output = vim.fn.system(table.concat(command, " "))
-	print(output)
+    local sfdxSubcommand = "project retrieve start"
+    local flagName = "--source-dir"
+    local filename = vim.fn.expand("%:t:r")
+    local command = { sfdxCommand, sfdxSubcommand, flagName, filename }
+    print("retrieving...")
+    local output = vim.fn.system(table.concat(command, " "))
+    print(output)
 end
 
 function M.getDefaultUserName()
-	local sfdxFile = '.sfdx/sfdx-config.json'
-	local sfdxConfigFile = vim.fn.findfile(sfdxFile, ".;")
-	if vim.fn.filereadable(sfdxConfigFile) == 0 then return "" end
-	local sfdxConfiguration = vim.fn.json_decode(vim.fn.readfile(sfdxConfigFile))
-	local defaultUserName = sfdxConfiguration["defaultusername"]
-	if defaultUserName == nil then return "" end
-	return defaultUserName
+    local sfdxFile = ".sf/config.json"
+    local sfdxConfigFile = vim.fn.findfile(sfdxFile, ".;")
+    if vim.fn.filereadable(sfdxConfigFile) == 0 then
+        return ""
+    end
+    local sfdxConfiguration = vim.fn.json_decode(vim.fn.readfile(sfdxConfigFile))
+    local defaultUserName = sfdxConfiguration["target-org"]
+    if defaultUserName == nil then
+        return ""
+    end
+    return defaultUserName
 end
 
 return M
