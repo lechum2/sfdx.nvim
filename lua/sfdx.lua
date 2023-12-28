1aca5149537004ef93355f06a13c48d3903cbcd0
Sebastian Lech <lechum2@gmail.com>
2023-12-28 15:42:51 +0100

    Use vim notification api

--- lua/sfdx.lua
+++ lua/sfdx.lua
@@ -5,31 +5,32 @@ local sfdxCommand = "sf"
 function M.deployCurrentFile()
     local sfdxDeployCommand = "project deploy start"
     local sfdxDeployPathParameter = "--source-dir"
-    local command = { sfdxCommand, sfdxDeployCommand, sfdxDeployPathParameter, vim.fn.expand("%") }
-    print("deploying...")
+    local filename = vim.fn.expand("%")
+    local command = { sfdxCommand, sfdxDeployCommand, sfdxDeployPathParameter, filename }
+    vim.notify("deploying file: " .. filename, vim.log.levels.INFO)
     local output = vim.fn.system(table.concat(command, " "))
-    print(output)
+    vim.notify(output, vim.log.levels.INFO)
 end
 
 function M.runTestsFromCurrentFile()
     local sfdxRunTestCommand = "apex test run"
     local sfdxRunTestClassParamter = "--class-names"
     local sfdxRunTestWaitParamter = "--wait 10"
-    local filename = vim.fn.expand("%:t:r")
+    local filename = vim.fn.expand("%")
     local command = { sfdxCommand, sfdxRunTestCommand, sfdxRunTestClassParamter, filename, sfdxRunTestWaitParamter }
-    print("running tests...")
+    vim.notify("running tests from file: " .. filename, vim.log.levels.INFO)
     local output = vim.fn.system(table.concat(command, " "))
-    print(output)
+    vim.notify(output, vim.log.levels.INFO)
 end
 
 function M.retrieveCurrentFile()
     local sfdxSubcommand = "project retrieve start"
     local flagName = "--source-dir"
-    local filename = vim.fn.expand("%:t:r")
+    local filename = vim.fn.expand("%")
     local command = { sfdxCommand, sfdxSubcommand, flagName, filename }
-    print("retrieving...")
+    vim.notify("retrieving file: " .. filename, vim.log.levels.INFO)
     local output = vim.fn.system(table.concat(command, " "))
-    print(output)
+    vim.notify(output, vim.log.levels.INFO)
 end
 
 function M.getDefaultUserName()
